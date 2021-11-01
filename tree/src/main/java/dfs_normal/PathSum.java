810ba1737db61e1aa35fa766d70484803bc47f30
liyang.zhang <2391415158@qq.com>
2021-11-01 13:58:10 +0800

    1

---
+++ tree/src/main/java/dfs_normal/PathSum.java
@@ -0,0 +1,21 @@
+package dfs_normal;
+
+import base.TreeNode;
+
+/**
+ * @Author tho
+ * @Date 2021/11/1/13:57
+ * @ProjectName Algorithms4th
+ * @ClassName: PathSum
+ * @Description: dfs 普通 路径总和
+ */
+public class PathSum {
+
+}
+class Solution112 {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+
+
+        return false;
+    }
+}
