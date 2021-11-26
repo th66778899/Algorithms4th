845108f0cb17f46b18c5c2929d767aafdf379e95
zly <2391415158@qq.com>
2021-11-26 13:58:01 +0800

    2021.10.25

--- backtracing/src/main/java/arrange/Permutations.java
+++ backtracing/src/main/java/arrange/Permutations.java
@@ -1,6 +1,9 @@
 package arrange;
 
+import com.sun.deploy.util.StringUtils;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -15,19 +18,64 @@ public class Permutations {
 class Solution46 {
     List<List<Integer>> res;
     List<Integer> path;
+    boolean[] used;
     public List<List<Integer>> permute(int[] nums) {
         res = new ArrayList<>();
         path = new ArrayList<>();
-
+        used = new boolean[nums.length];
+        backTracking(nums);
         return res;
     }
-    private void backTracking(int[] nums, int start) {
+    private void backTracking(int[] nums) {
         if (path.size() == nums.length) {
             res.add(new ArrayList<>(path));
         }
-        for (int i = start; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i] == true) {
+                continue;
+            }
+
+            used[i] = true;
             path.add(nums[i]);
-            backTracking(nums, i + 1);
+            backTracking(nums);
+            path.remove(path.size() - 1);
+            used[i] = false;
+
+        }
+
+    }
+
+
+}
+class Solution47 {
+    List<List<Integer>> res;
+    List<Integer> path;
+    boolean[] used;
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        res = new ArrayList<>();
+        path = new ArrayList<>();
+        used = new boolean[nums.length];
+        Arrays.sort(nums);
+        backTracking(nums);
+        return res;
+    }
+    private void backTracking(int[] nums) {
+        if (path.size() == nums.length) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1] && used[i - 1] == false) {
+                continue;
+            }
+            if (used[i] == true) {
+                continue;
+            }
+            used[i] = true;
+            path.add(nums[i]);
+            backTracking(nums);
+            path.remove(path.size() - 1);
+            used[i] = false;
 
         }
 
