ea49260fe4a87c7c0179ac10be03f4d1026c59e4
zly <2391415158@qq.com>
2021-12-23 15:32:34 +0800

    2021.12.17

--- dp/src/main/java/stock/Stock.java
+++ dp/src/main/java/stock/Stock.java
@@ -121,3 +121,47 @@ class Solution188 {
         System.out.println(new Solution188().maxProfit(2, new int[]{2, 4, 1}));
     }
 }
+class Solution309 {
+    public int maxProfit(int[] prices) {
+        int length = prices.length;
+
+        // dp[][] 数组有4个状态
+        // 0.保持买入状态
+        // 1.保持卖出状态(之前已经卖出,且度过了冷冻期)
+        // 2.保持卖出状态 (今天卖出)
+        // 3.处于冷冻期中
+        int[][] dp = new int[length][4];
+        dp[0][0] = -prices[0];
+
+        for (int i = 1; i < length; i++) {
+            dp[i][0] = Math.max(Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i]), dp[i - 1][3] - prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][3], dp[i - 1][1]);
+            dp[i][2] = dp[i - 1][0] + prices[i];
+            dp[i][3] = dp[i - 1][2];
+        }
+        int max = Math.max(Math.max(dp[length - 1][3], dp[length - 1][2]), dp[length - 1][1]);
+        return  max > 0 ? max : 0;
+    }
+
+    public static void main(String[] args) {
+        new Solution309().maxProfit(new int[]{1, 7, 2, 4});
+    }
+}
+class Solution714 {
+    public int maxProfit(int[] prices, int fee) {
+        int length = prices.length;
+        // dp[i][0] 不持有股票
+        // dp[i][1] 持有股票
+        int[][] dp = new int[length][2];
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[length - 1][0];
+    }
+
+    public static void main(String[] args) {
+        new Solution714().maxProfit(new int[]{1,3,2,8,4,9}, 2);
+    }
+}
