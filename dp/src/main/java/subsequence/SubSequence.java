861d7373fd7fb3acfaabebb0c5c44c2b084813da
zly <2391415158@qq.com>
2021-12-31 17:54:42 +0800

    2021.12.24

--- dp/src/main/java/subsequence/SubSequence.java
+++ dp/src/main/java/subsequence/SubSequence.java
@@ -18,14 +18,14 @@ class Solution300 {
         int res = 0;
         int[] dp = new int[nums.length];
         Arrays.fill(dp, 1);
-        for (int i = 1; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < i; j++) {
                 if (nums[i] > nums[j]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
-            }
-            if (dp[i] > res) {
-                res = dp[i];
+                if (dp[i] > res) {
+                    res = dp[i];
+                }
             }
         }
         return res;
@@ -42,9 +42,9 @@ class Solution674 {
         int[] dp = new int[length];
 
         Arrays.fill(dp, 1);
-        for (int i = 1; i < nums.length; i++) {
+        for (int i = 1; i < length; i++) {
             if (nums[i] > nums[i - 1]) {
-                dp[i] = dp[i- 1] + 1;
+                dp[i] = dp[i - 1] + 1;
             }
             res = Math.max(res, dp[i]);
         }
